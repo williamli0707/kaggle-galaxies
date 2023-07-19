97e25adf738fe111ed2c5c1422df9a4d611399ed
William Li <williamhli0707@gmail.com>
2023-07-19 12:38:51 -0700

    update theano package location

--- layers.py
+++ layers.py
@@ -2,7 +2,7 @@ import numpy as np
 import theano.tensor as T
 import theano
 from theano.tensor.signal.conv import conv2d as sconv2d
-from theano.tensor.signal.downsample import max_pool_2d
+from theano.tensor.signal.pool import max_pool_2d
 from theano.tensor.nnet.conv import conv2d
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 import sys
